196682a0c7c38c29b7a63f2cede6cb7711dac678
sxhthreo <951161604@qq.com>
2025-04-10 16:57:01 +0800

    init 2

---
+++ src/main/java/com/easyprep/mapper/QuestionBankQuestionMapper.java
@@ -0,0 +1,18 @@
+package com.easyprep.mapper;
+
+import com.easyprep.model.entity.QuestionBankQuestion;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author ustc
+* @description 针对表【question_bank_question(题库题目)】的数据库操作Mapper
+* @createDate 2025-04-08 19:31:32
+* @Entity com.easyprep.model.entity.QuestionBankQuestion
+*/
+public interface QuestionBankQuestionMapper extends BaseMapper<QuestionBankQuestion> {
+
+}
+
+
+
+
